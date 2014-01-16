87f610c1b7e2629120a2aeb09c313240ec2bd941
Panos Terzis <P.Terzis@ed.ac.uk>
2014-01-16 17:09:19 +0000

    add templates

---
+++ templates/footer.js
@@ -0,0 +1,10 @@
+define(function() {
+    return {
+        "download": {
+            "name": "Download",
+            "url": "download.html",
+            "class": "download-button",
+            "icon": "custom"
+        }
+    };
+});
